12e00f9625034e88a5553c26c15a54c1681ae7fc
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-02-22 14:05:34 +0530

    Add files via upload

---
+++ async-await.js
@@ -0,0 +1,14 @@
+const fetchmode = async ()=>{
+try{
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const todo = await response.json();
+    console.log(todo)
+}
+catch(error){
+    console.log(error.message)
+}
+    
+}
+
+fetchmode()
