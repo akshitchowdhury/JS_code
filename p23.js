4f1e646977fae137c6d8fe21fa9fac2e3c83a918
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-02-24 00:08:54 +0530

    Add files via upload

---
+++ p23.js
@@ -0,0 +1,16 @@
+
+// var line = "Swift"
+
+mode("Swift")
+
+function mode(line){
+
+var mid = line.substring(1,line.length-1)
+
+console.log(line.charAt(line.length-1)+mid+line.charAt(0))
+
+
+}
+
+
+
