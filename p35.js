6fd08dbe9a2e28f63c5cdb69da098bdc6132fa44
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-02-26 04:07:26 +0530

    Add files via upload

---
+++ p35.js
@@ -0,0 +1,36 @@
+
+// Write a JavaScript program to check whether the last digit of the three given positive integers is same.
+
+
+function checker(int1,int2,int3){
+
+
+    var st1 = int1.toString();
+    const final1 = st1.length-1;
+
+    var st2 = int2.toString();
+    const final2 = st2.length-1;
+    
+    var st3 = int3.toString();
+    const final3 = st3.length-1;
+
+
+    if((st1.charAt(final1)==st2.charAt(final2))&&(st2.charAt(final2)==st3.charAt(final3))&&(st3.charAt(final3)==st1.charAt(final1))){
+
+        console.log("They all are matching in the last digit.","The last digit matched is : ", st1.charAt(final1))
+    }
+
+    else{
+        console.log("Umatching units")
+    }
+    
+}
+
+checker(21,79321,905731)
+
+checker(2,79322,905731)
+
+checker(21,79321,9057316)
+
+checker(218759364,79324,905734)
+
