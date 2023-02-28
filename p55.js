f847f54ce66720a1e028b6cf33b1d38ca8b03c5c
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-03-01 04:25:25 +0530

    Add files via upload

---
+++ p55.js
@@ -0,0 +1,48 @@
+// Write a JavaScript program to check whether a given string contains equal number of p's and t's.  
+
+function pt(str){
+
+    // var str = "potato"
+    var arr = str.split("")
+
+    var pstore = ""
+    var tstore = ""
+    for(var i =0; i< arr.length;i++){
+
+        if(arr[i]=="p"){
+
+            pstore = pstore+arr[i]
+        }
+
+        else if(arr[i]=="t"){
+            tstore = tstore+arr[i]
+        }
+    }
+    
+    var plen = pstore.split("").length
+
+    var tlen = tstore.split("").length
+
+    if(plen==tlen){
+
+        console.log("Equal p's ad t's")
+    }
+
+    else{
+        console.log("unequal p's ad t's")
+    }
+}
+
+pt("potato")
+
+
+pt("potatop")
+
+
+pt("popatutatop")
+
+
+pt("pot")
+
+
+pt("potatoppp")
