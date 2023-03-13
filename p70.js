cbe4173dc3c1bbbcb3b9867e6734a56e0b241839
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-03-13 23:19:44 +0530

    Add files via upload

---
+++ p70.js
@@ -0,0 +1,21 @@
+
+
+function rotate(n1,n2,n3){
+
+
+    var arr = [n1,n2,n3]
+
+    for(var i = arr.length-1; i>=0; i--){
+
+        console.log(arr[i])
+    }
+    
+    
+    
+}
+
+rotate(10,2,50)
+
+// var arr = [11,23,45]
+
+// console.log(arr[arr.length-3])
