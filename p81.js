3c4d1ea09ad7ddad5f38c920c7af7207e373533a
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-03-21 23:53:10 +0530

    Add files via upload

---
+++ p81.js
@@ -0,0 +1,18 @@
+
+function add_array(n1,n2,n3,n4,n5){
+
+
+    var sum = 0
+    var arr = [n1,n2,n3,n4,n5]
+
+    for(var i=0; i<arr.length; i++){
+
+        sum = sum+arr[i]
+         
+    }
+
+    
+    console.log(sum)
+}
+
+add_array(1,1,1,1,1)
