1426bcbf39ffc1e37047ebbc2ab164ed23cdfbef
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-03-21 23:34:24 +0530

    Add files via upload

---
+++ p79.js
@@ -0,0 +1,23 @@
+function twice(num1,num2){
+    var arr = [num1,num2]
+
+    if(arr[0]==arr[1]){
+
+        return true
+    }
+
+    else{
+        return false
+    }
+
+
+}
+
+
+console.log(twice(30,30))
+
+
+console.log(twice(40,40))
+
+
+console.log(twice(30,40))
