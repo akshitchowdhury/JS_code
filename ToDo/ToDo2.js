b667bdffdd8ee7b03fa358c4a6fdfc8f180513d1
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-23 23:27:48 +0530

    todo10

--- ToDo/ToDo2.js
+++ ToDo/ToDo2.js
@@ -1,11 +1,11 @@
 
 
 
-var nodeList = document.getElementById("LI");
+var nodeList = document.getElementsByTagName("LI");
 
 var i;
 
-for(i=0 ; i< nodeList.clientHeight;i++){
+for(i=0 ; i< nodeList.length;i++){
 
     var span = document.createElement("SPAN");
     var textNode = document.createTextNode('\u00D7');
@@ -14,3 +14,34 @@ for(i=0 ; i< nodeList.clientHeight;i++){
     span.appendChild(textNode)
     nodeList[i].appendChild(span)
 }
+
+
+var close = document.getElementsByClassName("close");
+
+var i;
+for(i = 0; i< close.length; i++){
+
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = none;
+    }
+}
+
+
+var list = document.querySelector("ul");
+list.addEventListener('click', (ev)=>{
+
+    if(ev.target.value === 'LI'){
+        ev.target.classList.toggle("checked");
+    }
+
+}, false);
+
+
+
+
+
+var list = document.querySelector("li");
+var inputValue = document.getElementById("myInput").ariaValueMax;
+var t = document.createTextNode(inputValue);
+list.appendChild(t)
