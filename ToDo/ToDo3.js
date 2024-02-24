f265a4a16fba09c95f4268b3c2ee5ed6cf5127c9
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-24 23:55:04 +0530

    todo6

--- ToDo/ToDo3.js
+++ ToDo/ToDo3.js
@@ -38,6 +38,17 @@ for(i=0; i< nodeList.length; i++){
 
 }
 
+var close = document.getElementsByClassName("close");
+
+var i;
+for(i = 0; i< close.length; i++){
+
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
 
 
 
