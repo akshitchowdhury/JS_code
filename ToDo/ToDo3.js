bdaee5bff1055e3ed086d56ceb0cd69cb9911c8e
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-25 00:55:29 +0530

    todo8

--- ToDo/ToDo3.js
+++ ToDo/ToDo3.js
@@ -54,16 +54,15 @@ var close = document.getElementsByClassName("close");
 
 var i;
 
-for(i=0; i<close.length; i++){
+for(i=0; i< close.length;i++){
 
-        close[i].onclick = function(){
-            var div = this.parentElement;
-            div.style.display = "none"
-        }
+    close[i].onclick = function(){
 
+        var div = this.parentElement;
+
+        div.style.display = "none"
+    }
 }
-
-
 
 
 
